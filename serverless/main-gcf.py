27fed1f8a2fb7716aa0ae1d3da93dc5d59614a86
Daniel Whatmuff <danielwhatmuff@gmail.com>
2019-01-23 15:36:31 +0000

    removing serverless dir

--- serverless/main-gcf.py
+++
@@ -1,7 +0,0 @@
-def hello_you(request):
-    data = event['data']
-    name = data.decode('utf-8')
-    if not name:
-        name = 'anon'
-    return 'Hello {}'.format(name)
-
--- serverless/main.py
+++
@@ -1,7 +0,0 @@
-def hello_you(event, context):
-    data = event['data']
-    name = data.decode('utf-8')
-    if not name:
-        name = 'anon'
-    return 'Hello {}'.format(name)
-
